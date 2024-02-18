fe5db88bbb0d1f16ac332631c837a67ee9ff50b9
Shivam Shankar Pandey <95376442+shivamshankarpandey100@users.noreply.github.com>
2024-02-18 20:17:27 +0530

    Update p2.py

--- p2.py
+++ p2.py
@@ -5,6 +5,6 @@ pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
 # Example usage
 email = "user@example.com"
 if re.match(pattern, email):
-    print("Valid email address!")
+    print("Valid email address!.")
 else:
     print("Invalid email address.")
